dd6c0fd2233204805feaa26bec03852821cb6bd1
George Laptop <gkong88@WINDOWS-UDL6L4J.localdomain>
2019-09-22 00:33:06 -0700

    generating from snapshot data

--- Producer/eventGenerator.py
+++ Producer/eventGenerator.py
@@ -3,23 +3,60 @@ import pdb
 import confluent_kafka
 import time
 import random
+from datetime import datetime
+from dateutil.parser import parse
 
-# load an example json
-example = json.loads(open("shoppable_fit_example.json").read())
+class EventGenerator():
+    def __init__(self, csvPathString):
+        self.csvPathString = csvPathString
+        self.__readFile__(csvPathString)
 
-# service discovery
-broker = "ec2-35-160-75-159.us-west-2.compute.amazonaws.com:9092,ec2-52-25-251-166.us-west-2.compute.amazonaws.com:9092,ec2-52-32-113-202.us-west-2.compute.amazonaws.com:9092"      
-topic = "Viewed Shoppable Fit"
+    def __readFile__(self, csvPathString):
+        self.fid = open(csvPathString)
+        self.fid.readline() #discard header
+        self.streamEventTimeStart = parse(self.fid.readline().split(',')[1])
+        self.timerStart = datetime.now()
+        self.count = 0
+        
+    """
+    returns next shoppable post_id
 
-# load kafka config details
-conf = {'bootstrap.servers': broker}
+    blocks until sufficient time has passed
+    """
+    def get(self):
+        line = self.fid.readline()
+        self.count += 1
+        if len(line) == 0:
+            self.__readFile__()
+            line = self.fid.readline()
+        row = line.split(',')
+        key = row[0]
+        return key
 
-# initialize a connection to kafka producer
-p = Producer(**conf)
-
-while True:
-    time.sleep(0.1)
-    p.producer(topic, example, random.randint(100))
+def sendHTTPmessage():
+    pass
 
 
-pdb.set_trace()
+def main():
+    generator = EventGenerator("snapshot_posts.csv")
+    # load an example json
+    example = json.loads(open("shoppable_fit_example.json").read())
+    # service discovery
+    broker = "ec2-35-160-75-159.us-west-2.compute.amazonaws.com:9092,ec2-52-25-251-166.us-west-2.compute.amazonaws.com:9092,ec2-52-32-113-202.us-west-2.compute.amazonaws.com:9092"      
+    topic = "Viewed Shoppable Fit"
+    # load kafka config details
+    conf = {'bootstrap.servers': broker}
+    # initialize a connection to kafka producer
+    p = confluent_kafka.Producer(**conf)
+    counter = 0
+    while True:
+        key = generator.get()
+        # sendHTTPmessage(key, example)
+        p.produce(topic, example, key)
+        counter += 1
+        if counter % 20 == 0:
+            counter = 0
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
