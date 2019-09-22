50a95cf6d32562d99a56ed8060d3e4c4da9753c5
George Laptop <gkong88@WINDOWS-UDL6L4J.localdomain>
2019-09-22 14:16:50 -0700

    ksql create stream query, add some queries in a playbook

--- kafka/ksql/ViewedShoppableFit_01_key_postid.sql
+++ kafka/ksql/ViewedShoppableFit_01_key_postid.sql
@@ -3,11 +3,15 @@ DROP STREAM ViewedShoppableFit_01_key_postid;
 CREATE STREAM ViewedShoppableFit_01_key_postid 
     (
         event VARCHAR,
-        originalTimestamp VARCHAR,
+        timestamp VARCHAR,
         properties_display VARCHAR,
         properties_shoppable_post_id VARCHAR, 
         properties_origin VARCHAR
     )
     WITH (KAFKA_TOPIC = 'ViewedShoppableFit_00_raw_flatJSON',
           VALUE_FORMAT = 'JSON',
-          KEY = 'properties_shoppable_post_id');
+          KEY = 'properties_shoppable_post_id',
+          timestamp = 'timestamp',
+                            /* "2019-09-21T20:31:07.942Z" */
+           TIMESTAMP_FORMAT = 'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
+          );
---
+++ kafka/ksql/transient_query_playbook.sql
@@ -0,0 +1,4 @@
+select * from VIEWEDSHOPPABLEFIT_01_KEY_POSTID;
+
+select * from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
+where properties_display = 'full view';
