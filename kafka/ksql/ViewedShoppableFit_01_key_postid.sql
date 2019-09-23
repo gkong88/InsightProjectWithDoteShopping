da18a711325ee20ce1db570effe773e961558498
George Laptop <gkong88@WINDOWS-UDL6L4J.localdomain>
2019-09-22 20:51:34 -0700

    update sql queries

--- kafka/ksql/ViewedShoppableFit_01_key_postid.sql
+++ kafka/ksql/ViewedShoppableFit_01_key_postid.sql
@@ -1,5 +1,17 @@
 DROP STREAM ViewedShoppableFit_01_key_postid;
 
+CREATE STREAM foobar3
+    (
+        event VARCHAR,
+        properties STRUCT <
+            display VARCHAR,
+            shoppable_post_id VARCHAR>
+    )
+    WITH (KAFKA_TOPIC = 'ViewedShoppableFit_00_raw_JSON',
+          VALUE_FORMAT = 'JSON'
+    );
+
+
 CREATE STREAM ViewedShoppableFit_01_key_postid 
     (
         event VARCHAR,
@@ -17,3 +29,22 @@ CREATE STREAM ViewedShoppableFit_01_key_postid
            TIMESTAMP_FORMAT = 'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
             */
           );
+
+
+DROP STREAM ViewedShoppableFit_02_key_postid_full_views_only;
+
+CREATE STREAM ViewedShoppableFit_02_key_postid_full_views_only AS
+ select * from ViewedShoppableFit_01_key_postid
+ where PROPERTIES_DISPLAY = 'full view';
+    
+    
+    DROP STREAM ViewedShoppableFit_03_key_postid_previews_only;
+
+CREATE STREAM ViewedShoppableFit_03_key_postid_previews_only AS
+ select * from ViewedShoppableFit_01_key_postid
+ where PROPERTIES_DISPLAY = 'preview';
+    
+    
+    CREATE STREAM recast2 AS
+ select properties_display as disp, properties_shoppable_post_id as pid from ViewedShoppableFit_01_key_postid
+ where properties_display = 'full view';
--- kafka/ksql/transient_query_playbook.sql
+++ kafka/ksql/transient_query_playbook.sql
@@ -6,6 +6,8 @@ where properties_display = 'full view';
 
 drop table VIEWEDSHOPPABLEFIT_TABLE_most_view_30s;
 
+
+
 create table views_top10_5minutes as 
 select properties_shoppable_post_id, TOPK as views
 from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
@@ -26,14 +28,45 @@ select properties_shoppable_post_id, count(*) as views
 from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
 window tumbling (size 30 second) 
 group by properties_shoppable_post_id
-having count(*) > 1;
+having count(*) > 10;
 
-create table foo1 as 
+create table foo2 as 
 select properties_shoppable_post_id, count(*) as views
 from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
-window tumbling (size 30 second) 
+window tumbling (size 1 minute) 
 group by properties_shoppable_post_id
-having count(*) > 1;
+having count(*) > 100;
+
+create table foo3 as 
+select properties_shoppable_post_id, count(*) as views
+from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
+window tumbling (size 1 minute) 
+group by properties_shoppable_post_id
+having count(*) > 100;
+
+create table foo4_full_view_counter_1minute as 
+select properties_shoppable_post_id, count(*) as views
+from ViewedShoppableFit_02_key_postid_full_views_only
+window tumbling (size 1 minute) 
+group by properties_shoppable_post_id;
+
+create table foo4_full_view_counter_10s as 
+select properties_shoppable_post_id, count(*) as views
+from ViewedShoppableFit_02_key_postid_full_views_only
+window tumbling (size 10 seconds) 
+group by properties_shoppable_post_id;
+
+create table asdf3 as 
+select properties_shoppable_post_id
+from VIEWEDSHOPPABLEFIT_01_KEY_POSTID
+window tumbling (size 10 second) 
+having count(*) > 10;
+
+
+
+
+create stream foostream3 as select * from foo2;
+
 
 
 select properties_shoppable_post_id, views from VIEWEDSHOPPABLEFIT_TABLE_most_view_30sec;
