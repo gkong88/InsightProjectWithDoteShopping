2edc39f259ee99dbc96261dae97a2648f17ed5a0
George Laptop <gkong88@WINDOWS-UDL6L4J.localdomain>
2019-09-26 17:32:09 -0700

    clean up test files

--- segment_webhook_endpoint_kafka_producer/test.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-import KafkaRESTAPI
-# from KafkaRESTAPI.py import KafkaRESTAPI
-
-class TestTime(TestCase):
-    def test_segment_timestamp_to_unix_millis(self):
-        self.assertEqual(KafkaRESTAPI.segment_timestamp_to_unix_millis("2019-09-21T20:31:07.942Z"), 1569097867942)
-        print("passed timestamp conversion test")
-
