5935ff8160cf2215d0582b0fff438b5be089df06
George Laptop <gkong88@WINDOWS-UDL6L4J.localdomain>
2019-09-26 17:31:33 -0700

    add test UDF

--- kafka/ksql/ksql_playbook.sql
+++ kafka/ksql/ksql_playbook.sql
@@ -1,64 +1,56 @@
-CREATE STREAM trackViewedShoppableFit_01_stream
+CREATE STREAM CREATE_POST
     (
+        properties_shoppable_post_id BIGINT,
+        segment_timestamp BIGINT
+    )
+    WITH (KAFKA_TOPIC = 'trackCreatedStory_00_raw_flatJSON',
+          VALUE_FORMAT = 'JSON',
+          KEY='properties_shoppable_post_id',
+          TIMESTAMP ='segment_timestamp'
+          );
+
+CREATE STREAM CLICK
+    (
+        properties_shoppable_post_id BIGINT,
+        segment_timestamp BIGINT,
         properties_display VARCHAR
     )
     WITH (KAFKA_TOPIC = 'trackViewedShoppableFit_00_raw_flatJSON',
-          VALUE_FORMAT = 'JSON'
+          VALUE_FORMAT = 'JSON',
+          KEY='properties_shoppable_post_id',
+          TIMESTAMP ='segment_timestamp'
+          );
+
+CREATE STREAM clicks_w_post_timestamp as
+select CREATE_POST.segment_timestamp as segment_timestamp, CREATE_POST.properties_shoppable_post_id as properties_shoppable_post_id ,
+        CLICK.properties_display as properties_display
+        FROM CREATE_POST JOIN CLICK WITHIN 3 DAYS
+        ON CREATE_POST.properties_shoppable_post_id = CLICK.properties_shoppable_post_id;
+
+
+create table clicks_w_post_timestamp_day_counter AS
+select post_id, segment_timestamp,
+        count(*) as total,
+        SUM(CASE WHEN properties_shoppable_post_id = 'preview' or properties_display = 'thumbnail'THEN 1 ELSE 0 END) as preview,
+        SUM(CASE WHEN properties_shoppable_post_id = 'detail' or properties_display = 'full_view' THEN 1 ELSE 0 END) as full_view
+        from clicks_w_post_timestamp
+        WINDOW HOPPING (SIZE 3 DAYS, ADVANCE BY 1 minute )
+        group by post_id, segment_timestamp;
+
+CREATE STREAM clicks_w_post_timestamp_day_counter_report_stream
+(
+        post_id BIGINT,
+        TOTAL BIGINT,
+        PREVIEW INT,
+        FULL_VIEW INT
+    )
+    WITH (KAFKA_TOPIC = 'clicks_w_post_timestamp_day_counter',
+          VALUE_FORMAT = 'JSON',
+          PARTITIONS = 1,
+          REPLICAS = 1
           );
 
 
 
 
-CREATE TABLE trackViewedShoppableFit_02_previews_last_10min as 
-select rowkey as shoppable_post_id, count(*) as previews
-from trackViewedShoppableFit_01_stream
-window hopping (size 10 minutes, advance by 2 minute)
-where properties_display = 'preview'
-group by rowkey;
-
-CREATE TABLE trackViewedShoppableFit_02_fullviews_last_10min as 
-select rowkey as shoppable_post_id, count(*) as fullviews
-from trackViewedShoppableFit_01_stream
-window hopping (size 10 minutes, advance by 2 minute)
-where properties_display = 'detail'
-group by rowkey;
-
-CREATE TABLE trackViewedShoppableFit_03_previews_last_hour as 
-select rowkey as shoppable_post_id, count(*) as previews
-from trackViewedShoppableFit_01_stream
-window hopping (size 60 minutes, advance by 10 minute)
-where properties_display = 'preview'
-group by rowkey;
-
-CREATE TABLE trackViewedShoppableFit_03_fullviews_last_hour as 
-select rowkey as shoppable_post_id, count(*) as fullviews
-from trackViewedShoppableFit_01_stream
-window hopping (size 60 minutes, advance by 10 minute)
-where properties_display = 'detail'
-group by rowkey;
-
-CREATE TABLE trackViewedShoppableFit_04_previews_day as 
-select rowkey as shoppable_post_id, count(*) as previews
-from trackViewedShoppableFit_01_stream
-window hopping (size 24 hours, advance by 6 hours)
-where properties_display = 'preview'
-group by rowkey;
-
-CREATE TABLE trackViewedShoppableFit_04_fullviews_day as 
-select rowkey as shoppable_post_id, count(*) as fullviews
-from trackViewedShoppableFit_01_stream
-window hopping (size 24 hours, advance by 6 hours)
-where properties_display = 'detail'
-group by rowkey;
-
-
-CREATE TABLE hottest_posts_15_min4 as 
-select event PROPERTIES_SHOPPABLE_POST_ID, count(*)*100000000 +  PROPERTIES_SHOPPABLE_POST_ID as count_post
-from ViewedShoppableFit_01_key_postidKLUGE
-window tumbling (size 15 minute) 
-where properties_display = 'full view'
-group by event, PROPERTIES_SHOPPABLE_POST_ID;
-
-
-SELECT TOPK(count_post, 5) FROM hottest_posts3 WINDOW TUMBLING (size 15 minute) group by event;
-
+-- SCRATCH SHEET --
