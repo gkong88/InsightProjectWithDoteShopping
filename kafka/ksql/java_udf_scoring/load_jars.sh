5b0a0c2d003eb173acc9ab6197968c6bfc0ec57c
Ubuntu <ubuntu@ip-10-0-0-137.us-west-2.compute.internal>
2019-09-27 06:35:23 +0000

    add simple script to load jars for ksql

---
+++ kafka/ksql/java_udf_scoring/load_jars.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo cp ./target/'ksql-udf-demo-1.0.jar' /etc/ksql/ext/'ksql-udf-demo-1.0.jar'
+sudo cp ./target/'ksql-udf-demo-1.0-jar-with-dependencies.jar' /etc/ksql/ext/'ksql-udf-demo-1.0-jar-with-dependencies.jar'
