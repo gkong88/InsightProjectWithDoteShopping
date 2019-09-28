9577416f6489bae8a5cad464a1617d629daee4c4
Ubuntu <ubuntu@ip-10-0-0-170.us-west-2.compute.internal>
2019-09-28 06:56:00 +0000

    add mkdir command before cp

--- kafka/ksql/java_udf_scoring/load_jars.sh
+++ kafka/ksql/java_udf_scoring/load_jars.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+sudo mkdir -p /etc/ksql/ext
 sudo cp ./target/'ksql-udf-demo-1.0.jar' /etc/ksql/ext/'ksql-udf-demo-1.0.jar'
 sudo cp ./target/'ksql-udf-demo-1.0-jar-with-dependencies.jar' /etc/ksql/ext/'ksql-udf-demo-1.0-jar-with-dependencies.jar'
