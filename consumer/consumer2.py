d397d39242aaccc25110366f2455011c145e2945
George Ong <gkong88@gmail.com>
2019-09-28 23:42:25 -0700

    refactor fns

--- consumer/consumer2.py
+++ consumer/consumer2.py
@@ -1,5 +1,4 @@
 import json
-import csv
 from kafka import KafkaConsumer
 import sys
 import smart_open
@@ -7,24 +6,22 @@ import time
 import datetime
 import pdb
 
-def push_to_s3(msgs):
-    csvfile = open('test.txt', 'w')
+def get_scores(msgs):
     scores = {}
     counter = 0
     for m in msgs:
         if m != None:
             scores[m.value['PROPERTIES_SHOPPABLE_POST_ID']] = m.value['RT_SCORE']
             counter += 1
-    pdb.set_trace()
-    # with smart_open.smart_open('s3://dote-fit-scores/calculated_score_2/rtscore_' +  datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '_.csv', 'wb') as fout:
-    #     fout.write('shoppable_post_id,score\n')
-    #     csvfile.write('shoppable_post_id,score\n')
-    #     fout.write(','.join(map(str, [m.value['PROPERTIES_SHOPPABLE_POST_ID'],
-    #                                   int(.value['RT_SCORE'])])) + '\n')
-    #     csvfile.write(','.join(map(str, [.value['PROPERTIES_SHOPPABLE_POST_ID'],
-    #                                      int(.value['RT_SCORE'])])) + '\n')
-    #     csvfile.close()
+            if counter % 1000 == 0:
+                print("event processing counter: %s" % counter)
+    return scores;
 
+def push_to_s3(scores):
+    with smart_open.smart_open('s3://dote-fit-scores/calculated_score_2/rtscore_' +  datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '_.csv', 'wb') as fout:
+        fout.write('shoppable_post_id,score\n')
+        for post, score in scores.items():
+            fout.write(str(post) + ',' + str(int(score)))
 
 def reset_consumer(c, time_window):
     pdb.set_trace()
@@ -34,9 +31,7 @@ def reset_consumer(c, time_window):
     c.seek(p, start_offset)
     return end_offset
 
-from kafka import KafkaConsumer
-import json
-import time
+
 time_window = 3 * 24 * 60 * 60 * 1000
 topic_name = 'CLICK__FI_RECENT_POST__AG_COUNTS__EN_SCORE2'
 servers = 'ec2-100-20-18-195.us-west-2.compute.amazonaws.com:9092'
@@ -48,11 +43,9 @@ c = KafkaConsumer(topic_name,
                   group_id='my-group',
                   value_deserializer=lambda x: json.loads(x.decode('utf-8'))
                   )
-m = c.poll(5, 1)
-while len(m) == 0: m = c.poll(5,1)
+m = c.poll(1,1)
+while len(m) == 0: m = c.poll(1,1)
 while True:
-    # try:
     reset_consumer(c, time_window)
-    push_to_s3(c)
-    # except:
-        # print("failed snapshot attempt")
+    scores = get_scores(c)
+    push_to_s3(scores)
