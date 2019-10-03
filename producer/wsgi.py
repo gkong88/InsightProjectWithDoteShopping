ac6b7d162c9f77b26b6fa2f93ea3dd3b8d6c4e04
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2019-10-03 06:12:25 +0000

    update import to reflect name change of producer py

--- producer/wsgi.py
+++ producer/wsgi.py
@@ -1,4 +1,4 @@
-from kafka_ingest import application
+from segment_ingest import application
 if __name__ == "__main__":
     print("RUNNING SERVER")
     application.run(host='0.0.0.0', port = 5000)
