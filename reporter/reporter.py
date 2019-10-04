e3e74ec873605f912fece6c1332932e5ff9d89e7
George Ong <gkong88@gmail.com>
2019-10-03 20:51:16 -0700

    add bottom feeder fn

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -1,21 +1,17 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_daq as daq
-import dash_table
 import pandas as pd
-import plotly.graph_objs as go
-from dash.dependencies import Input, Output
 from kafka import KafkaConsumer
-import smart_open
 import json
-from scoring_function_creator import ScoringFunctionCreator
-from recent_posts_table import RecentPostsTable
+from scoring_function import ScoringFunction
+from live_table import RecentPostsTable
 import time
 import datetime
 from pytz import timezone
+import threading
 
-def start_posts_table():
+class Reporter:
+
+
+def init_posts_table():
     # config variables
     topic_name = 'CLICK__FI_RECENT_POST__AG_COUNTS__EN_SCORE2'
     servers = 'ec2-100-20-18-195.us-west-2.compute.amazonaws.com:9092'
@@ -27,13 +23,29 @@ def start_posts_table():
                              enable_auto_commit=True,
                              group_id='my-group',
                              value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-    scoring_function = ScoringFunctionCreator()
-    return RecentPostsTable(consumer, scoring_function)
+    scoring_function = ScoringFunction()
+    return RecentPostsTable(consumer, scoring_function, datetime.timedelta(days = 1))
 
-def update_posts():
-    pass
+
+def update_posts(lock, live_posts_table):
+    """
+    Bottom feeder to update posts when no reports are scheduled.
+
+    :param lock:
+    :param live_posts_table:
+    :return:
+    """
+    while True:
+        lock.acquire()
+        live_posts_table.update()
+        lock.release()
+
+
+def push_s3(min_interval: datetime.timedelta = datetime.timedelta(minutes = 2)):
+
 
 if __name__ == "__main__":
-    start_posts_table()
+    lock = threading.Lock() #lock for live_posts_table
+    live_posts_table = init_posts_table()
 
 
