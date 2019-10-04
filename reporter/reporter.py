78c8d5c59ed290940d1dad42f4871778790d5ce2
Ubuntu <ubuntu@ip-10-0-0-51.us-west-2.compute.internal>
2019-10-04 07:20:54 +0000

    running!

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -2,7 +2,7 @@ import pandas as pd
 from kafka import KafkaConsumer
 import json
 from scoring_function import ScoringFunction
-from live_table import RecentPostsTable
+from live_table import LiveTable
 import datetime
 import threading
 import requests
@@ -16,28 +16,28 @@ class Reporter:
     """
     # Concurrency is managed with monitor pattern (locking) on methods that
     # access the table state.
-    def __init__(self, topic_name: str, servers: str,
+    def __init__(self, topic_name: str, kafka_servers: str,
                  output_topic_name: str, kafka_rest_proxy_server: str,
                  scoring_function: ScoringFunction = ScoringFunction(),
                  # s3_min_push_interval: datetime.timedelta = datetime.timedelta(minutes = 2),
-                 min_push_interval: datetime.timedelta = datetime.timedelta(seconds = 3)
+                 min_push_interval: datetime.timedelta = datetime.timedelta(seconds = 2)
                  # s3_topic_name: str = "s3_report"
                  ):
         # init kafka consumer
         consumer = KafkaConsumer(topic_name,
-                                 bootstrap_servers=servers,
+                                 bootstrap_servers=kafka_servers,
                                  auto_offset_reset='earliest',
                                  enable_auto_commit=True,
                                  group_id='my-group',
                                  value_deserializer=lambda x: json.loads(x.decode('utf-8')))
         # init live table
-        self.table = RecentPostsTable(consumer, scoring_function, datetime.timedelta(days = 1))
+        self.table = LiveTable(consumer, datetime.timedelta(days = 1), scoring_function)
 
         # store timing variables
         # self.s3_min_push_interval = s3_min_push_interval
         self.min_push_interval = min_push_interval
-        self.last_push_timestamp = 0
-        self.next_push_timestamp = 0
+        self.last_push_timestamp = datetime.datetime(1970, 1, 1)
+        self.next_push_timestamp = datetime.datetime(1970, 1, 1)
         # self.s3_topic_name = s3_topic_name
         self.ui_topic_name = output_topic_name
 
@@ -49,12 +49,11 @@ class Reporter:
         self.headers = {"Content-Type": "application/vnd.kafka.json.v2+json", "Accept": "application/vnd.kafka.v2+json",
                    "Connection": 'close'}
 
-
     def run(self):
         self.threads.append(threading.Thread(target = self.update_table_forever))
         # self.threads.append(threading.Thread(self.push_s3_forever))
         self.threads.append(threading.Thread(target = self.push_snapshot_forever))
-        for thread in self.threads():
+        for thread in self.threads:
             thread.start()
 
     def update_scoring_function(self):
@@ -77,6 +76,7 @@ class Reporter:
             self.lock.acquire()
             self.table.update()
             self.lock.release()
+            print("updated")
 
     # def push_s3_forever(self):
     #     while True:
@@ -93,12 +93,13 @@ class Reporter:
             while datetime.datetime.now() < self.next_push_timestamp:
                 sleep_duration = max((self.next_push_timestamp - datetime.datetime.now()).seconds, 1)
                 time.sleep(sleep_duration)
-            self.next_push_timestamp = datetime.datetime.now() + self.min_push_interval
 
             kafka_payload = {"records": [{"value": posts}]}
             response = requests.post(self.destination_url, json=kafka_payload, headers=self.headers)
             response.raise_for_status()
             response.close()
+            self.next_push_timestamp = datetime.datetime.now() + self.min_push_interval
+            print("pushed snapshot")
 
     # def push_snapshot(self, min_interval: datetime.timedelta = datetime.timedelta(minutes = 2)):
     #     # TODO
@@ -112,11 +113,12 @@ class Reporter:
 if __name__ == "__main__":
     topic_name = 'CLICK__FI_RECENT_POST__AG_COUNTS__EN_SCORE2'
     kafka_servers = 'ec2-100-20-18-195.us-west-2.compute.amazonaws.com:9092'
-    output_topic_name = "ui_report",
+    output_topic_name = "ui_report"
     kafka_rest_proxy_server = "http://ec2-52-36-231-83.us-west-2.compute.amazonaws.com:8082"
     reporter = Reporter(topic_name = topic_name,
                         kafka_servers = kafka_servers,
                         output_topic_name = output_topic_name,
                         kafka_rest_proxy_server = kafka_rest_proxy_server)
+    reporter.run()
 
 
