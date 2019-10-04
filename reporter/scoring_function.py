a465e584dd2d3e30d1f6fb61d8af00e758966c94
George Ong <gkong88@gmail.com>
2019-10-03 20:27:34 -0700

    add comments

--- reporter/scoring_function.py
+++ reporter/scoring_function.py
@@ -1,9 +1,22 @@
 import math
 
 
-class ScoringFunctionCreator:
+class ScoringFunction:
+    """
+    This class is used to construct a parameterizable function that returns
+    a score based on previews and full_views.
+    """
     def __init__(self, max_coldness_score=50, min_previews_threshold=40, cold_threshold_steepness=0.35,
                  max_hotness_score=50, ctr_hotness_threshold=0.12, hot_threshold_steepness=17):
+        """
+
+        :param max_coldness_score: max points possible for cold_start posts
+        :param min_previews_threshold: soft threshold for when cold_start weighting declines (50% of max value)
+        :param cold_threshold_steepness: steepness of sigmoid function
+        :param max_hotness_score: max points possible for high click through rate posts
+        :param ctr_hotness_threshold: soft threshold for when hotness weighting increases(50% of max value)
+        :param hot_threshold_steepness: steepness of sigmoid function
+        """
         self.max_coldness_score = max_coldness_score
         self.min_previews_threshold = min_previews_threshold
         self.cold_threshold_steepness = cold_threshold_steepness
