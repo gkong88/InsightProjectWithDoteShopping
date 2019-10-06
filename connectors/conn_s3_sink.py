2e5ff5e79e98f8aebf0d11503091d2428bd2d499
George Ong <gkong88@gmail.com>
2019-10-06 11:06:13 -0700

    implemented

--- connectors/conn_s3_sink.py
+++ connectors/conn_s3_sink.py
@@ -1,49 +1,115 @@
-import pandas as pd
-from kafka import KafkaConsumer
+import smart_open
+from kafka import KafkaConsumer, KafkaProducer, TopicPartition
 import json
-from scoring_function import ScoringFunction
 import datetime
-import threading
+from pytz import timezone
 import time
 import os, sys
 sys.path.insert(0, os.path.abspath('../util'))
 from utility import RepeatPeriodically, heartbeat
 
+
 class S3SinkConnector:
-    def __init__(self, input_topic_name: str, kafka_servers: str,
-                 min_push_interval: datetime.timedelta = datetime.timedelta(minutes = 2),
+    """
+    Sink Connector to S3 from stream analysis.
 
+    """
+    def __init__(self, input_topic_name: str, bootstrap_servers: list[str],
+                 s3_bucket_path: str,
+                 log_topic_name: str,
+                 min_push_interval: datetime.timedelta = datetime.timedelta(minutes=2)
                  ):
-        # init kafka consumer
-        consumer = KafkaConsumer(input_topic_name,
-                                 bootstrap_servers=kafka_servers,
-                                 auto_offset_reset='earliest',
-                                 enable_auto_commit=True,
-                                 value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-        # init live table
-        self.table = LiveTable(consumer, datetime.timedelta(days = 1), scoring_function)
+        self.consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers,
+                                      auto_offset_reset='latest',
+                                      enable_auto_commit=True,
+                                      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        partition_number = list(self.consumer.partitions_for_topic(input_topic_name))[0]
+        self.topic_partition = TopicPartition(input_topic_name, partition_number)
+        self.consumer.assign([self.topic_partition])
 
-        # store timing variables
+        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                                      value_serializer=lambda x: json.dumps(x))
+        self.log_topic_name = log_topic_name
+
+        self.s3_bucket_path = s3_bucket_path
         self.min_push_interval = min_push_interval
-        self.next_push_timestamp = datetime.datetime(1970, 1, 1)
 
-        # init lock for coordinating update and push events.
-        self.lock = threading.Lock()
-        self.threads = []
+    def run_forever(self):
+        while True:
+            # poll kafka topic until consumer partition is automatically generated
+            message = self.__get_latest_message()
+            if message is None:
+                continue
 
-        self.destination_url = kafka_rest_proxy_server + "/topics/" + output_topic_name
-        self.headers = {"Content-Type": "application/vnd.kafka.json.v2+json", "Accept": "application/vnd.kafka.v2+json",
-                   "Connection": 'close'}
+            # push latest scores to s3, log this push
+            last_push_filename, last_push_timestamp = self.__push_to_s3(message)
+            self.__log_push_to_s3(message, key = last_push_filename)
 
-def get_latest():
-    pass
+            # wait until the next push interval
+            next_push_timestamp = last_push_timestamp + self.min_push_interval
+            while datetime.datetime.now() < next_push_timestamp:
+                sleep_duration = max((next_push_timestamp - datetime.datetime.now()).seconds, 1)
+                print("sleeping until %s" % next_push_timestamp)
+                print("sleeping for %s seconds" % sleep_duration)
+                time.sleep(sleep_duration)
 
-def push_s3(self):
+    def __push_to_s3(self, message) -> datetime.datetime:
+        """
+        Push scores to s3 bucket.
+        """
+        # Assumes credentials are located in:
+        #   ~/.aws/credentials
+        scores = message.value
+
+        push_timestamp = datetime.datetime.now()
+        push_timestamp_pst_strf = push_timestamp.astimezone(timezone('US/Pacific')).strftime('%Y%m%d%H%M%S')
+
+        print("Pushing data to S3 at: %s" % push_timestamp_pst_strf)
+        s3_filename = self.s3_bucket_path + 'rtscore_' + push_timestamp_pst_strf + '.csv'
+        with smart_open.open(s3_filename, 'wb') as fout:
+            fout.write(b'shoppable_post_id,score\n')
+            for post, values in scores.items():
+                fout.write((str(post) + ',' + str(round(values['score'])) + '\n').encode('utf-8'))
+        print("Push successful!")
+        return s3_filename, push_timestamp
+
+    def __log_push_to_s3(self, message, key):
+        self.producer.send(topic=self.log_topic_name, value=message.value)
+
+    def __get_latest_message(self):
+        """
+
+        :return: latest message from topic
+
+        returns None on failure.
+        """
+        # poll until new data is added to topic
+        while self.consumer.end_offsets([self.topic_partition])[self.topic_partition] == \
+                self.consumer.position(self.topic_partition):
+            time.sleep(secs=1)
+        self.consumer.seek_to_end(self.topic_partition)
+        try:
+            return self.consumer.poll(timeout_ms = 5000, max_records = 1)[self.topic_partition][0]
+        except:
+            return None
+
 
 if __name__ == "__main__":
-    topic_name = 'CLICK__FI_RECENT_POST__AG_COUNTS'
-    kafka_servers = ['ec2-100-20-18-195.us-west-2.compute.amazonaws.com:9092',
+    input_topic_name = 'CLICK__FI_RECENT_POST__AG_COUNTS'
+    bootstrap_servers = ['ec2-100-20-18-195.us-west-2.compute.amazonaws.com:9092',
                      'ec2-100-20-8-59.us-west-2.compute.amazonaws.com:9092',
                      'ec2-100-20-75-14.us-west-2.compute.amazonaws.com:9092']
-    heartbeat_kwargs = {'bootstrap_servers': kafka_servers, 'topic_name': 'pipeline_logs', 'key': 'conn_s3_sink'}
+    s3_bucket_path = 's3: // dote - fit - scores / calculated_score_2 /'
+    log_topic_name = 'connector_s3_sink_push_log'
+    min_push_interval = datetime.timedelta(minutes=2)
+
+    heartbeat_kwargs = {'bootstrap_servers': bootstrap_servers, 'topic_name': 'pipeline_logs', 'key': 'conn_s3_sink'}
     RepeatPeriodically(fn=heartbeat, interval=300, kwargs=heartbeat_kwargs).run()
+
+    sink = S3SinkConnector(input_topic_name = input_topic_name,
+                           bootstrap_servers = bootstrap_servers,
+                           s3_bucket_path = s3_bucket_path,
+                           log_topic_name = log_topic_name,
+                           min_push_interval = min_push_interval)
+    sink.run_forever()
+
