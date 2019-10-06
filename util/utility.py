4eace03fab343c991b544b447bd7cb82f386ab23
George Ong <gkong88@gmail.com>
2019-10-05 17:21:57 -0700

    rename util to utility, add key param to heartbeat

---
+++ util/utility.py
@@ -0,0 +1,38 @@
+from kafka import KafkaProducer
+from typing import Callable
+import threading
+import time
+
+
+
+class RepeatPeriodically:
+    """
+    Class that periodically executes a function at a specified interval
+    """
+    def __init__(self, fn: Callable, interval: float, kwargs: dict):
+        """
+
+        :param fn: function to be periodically executed
+        :param interval: interval in seconds
+        :param kwargs: keyword arguments for function
+        """
+        self.interval = interval
+        self.fn = fn
+        self.kwargs = kwargs
+
+    def run(self):
+        self.fn(**self.kwargs)
+        threading.Timer(self.interval, self.run).start()
+
+def heartbeat(bootstrap_servers, topic_name, key):
+    """
+    Sends a heartbeat to a kafka topic.
+    Used for logging
+
+    :param bootstrap_servers: addresses of kafka bootstrap servers
+    :param key: kafka event key
+    :param topic_name:
+    :return:
+    """
+    p = KafkaProducer(bootstrap_servers = bootstrap_servers)
+    p.send(topic = topic_name, timestamp_ms = int(time.time() * 1000), key = key)
