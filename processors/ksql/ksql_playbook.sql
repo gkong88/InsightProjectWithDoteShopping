674b6418c27895d57ef8a583ba477a2a1665fa92
George Ong <gkong88@gmail.com>
2019-10-17 23:19:22 -0700

    add ingest timestamp

--- processors/ksql/ksql_playbook.sql
+++ processors/ksql/ksql_playbook.sql
@@ -10,6 +10,7 @@ CREATE STREAM CLICK
     (
         properties_shoppable_post_id BIGINT,
         segment_timestamp BIGINT,
+        ingest_timestamp BIGINT,
         properties_display VARCHAR
     )
     WITH (KAFKA_TOPIC = 'trackViewedShoppableFit_00_raw_flatJSON',
@@ -33,7 +34,8 @@ CREATE STREAM CREATE_POST
 
 -- click streams (FI)ltered by recent posts, enriched by post creation timestamp
 CREATE STREAM CLICK__FI_RECENT_POST with (timestamp = 'post_timestamp') as
-select CREATE_POST.segment_timestamp as post_timestamp, CLICK.segment_timestamp as click_timestamp, CREATE_POST.properties_shoppable_post_id as properties_shoppable_post_id ,
+select CREATE_POST.segment_timestamp as post_timestamp, CLICK.segment_timestamp as click_timestamp, 
+        CLICK.ingest_timestamp as ingest_timestamp, CREATE_POST.properties_shoppable_post_id as properties_shoppable_post_id ,
         CLICK.properties_display as properties_display
         FROM CREATE_POST LEFT JOIN CLICK WITHIN 36 HOURS
         ON CREATE_POST.properties_shoppable_post_id = CLICK.properties_shoppable_post_id
@@ -44,6 +46,7 @@ select CREATE_POST.segment_timestamp as post_timestamp, CLICK.segment_timestamp 
 create table CLICK__FI_RECENT_POST__AG_COUNTS with (timestamp = 'post_timestamp') AS
 select properties_shoppable_post_id, post_timestamp, 
         MAX(click_timestamp) as last_click_timestamp,
+        MAX(last_click_timestamp) as last_click_timestamp,
         count(*) as total,
         SUM(CASE WHEN properties_display = 'preview' or properties_display = 'thumbnail' THEN 1 ELSE 0 END) as preview,
         SUM(CASE WHEN properties_display = 'detail' or properties_display = 'full_view' THEN 1 ELSE 0 END) as full_view
